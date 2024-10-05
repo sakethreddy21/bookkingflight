ced0c3a2eb6f9e60a5c9491cde6379e0a9539417
saketh <saketh.reddy@trainingmug.com>
2024-10-05 19:47:09 +0530

    assignment completed

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-20 font-[family-name:var(--font-geist-sans)] ">
-      <div className="text-[#000C0B] text-[36px] font-normal flex flex-row  justify-center items-center">{greeting!==''? `${greeting}, Brain`:(<Skeleton className="h-12 w-[350px]"/>)}</div>
+      <div className="text-[#000C0B] text-[36px] font-normal flex flex-row  justify-center items-center">{greeting!==''? `${greeting}, Brian`:(<Skeleton className="h-12 w-[350px]"/>)}</div>
 
       <div className="h-[265px] border-[#E6E8EB] border-[1px] w-[80%] mt-[30px] rounded-xl p-[24px] shadow-sm">
         <SearchBar />
